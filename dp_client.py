3035425088fb57cfce4d83a9b4a82f7002e23522
dbratu777 <dab755@msstate.edu>
2025-02-21 04:27:55 +0000

    update databasae locations and static ips£

--- dp_client.py
+++ dp_client.py
@@ -6,42 +6,6 @@ import base64
 import os
 import websockets
 
-"""
-# this should be included in whatever script is passing data to dp_client.py (alert_json is passed to it as alert data)
-import datetime
-import json
-from sqlalchemy import create_engine, Column, Integer, Float, String, DateTime, Boolean
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-# ALERT INFO: 
-# Types: 0 = Temp, 1 = pH, 2 = ORP, 3 = Fish Health
-class Alert(Base):
-    __tablename__ = 'alerts'
-    id = Column(Integer, primary_key=True)
-    type = Column(Integer, nullable=False)
-    title = Column(String(100), nullable=False)
-    description = Column(String(200), nullable=True)
-    timestamp = Column(DateTime, default=datetime.datetime.now(datetime.timezone.utc))
-    read = Column(Boolean, default=False)
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'type': self.type,
-            'title': self.title,
-            'description': self.description,
-            'timestamp': self.timestamp.isoformat(),
-            'read': self.read
-        }
-    
-# example usage: 
-alert = Alert(type=3, title="Fish Health", description="Example Desc.")
-alert_dict = alert.to_dict()
-alert_json = json.dumps(alert_dict)
-"""
-
 async def send_alert(websocket, message):
     await websocket.send(f'TEXT:{message}')
 
@@ -57,7 +21,7 @@ async def main():
     parser.add_argument("data", help="Alert Text or File Path to Heatmap")
     args = parser.parse_args()
 
-    server_addr = "ws://localhost:2777"
+    server_addr = "ws://192.168.1.77:2777"
     try: 
         async with websockets.connect(server_addr) as websocket:
             if args.type == "alert":
--- rp_client.py
+++ rp_client.py
@@ -1,17 +1,30 @@
 # raspberry pi client socket
 
+import argparse
 import asyncio
 import base64
+import os
 import websockets
 
-async def send_message():
-    server_addr = "ws://localhost:1777"
-    try:
+async def send_image(websocket, image_file_path):
+    with open(image_file_path, "rb") as image_file:
+        image_data = image_file.read()
+        image_data = base64.b64encode(image_data).decode("utf-8")
+        await websocket.send(f'IMAGE:{image_data}')
+
+async def main():
+    parser = argparse.ArgumentParser(description="Fish Friend's Web Server Client for Forwarding Fish Images to the Data Processing Server.")
+    parser.add_argument("data", help="File Path to Image")
+    args = parser.parse_args()
+
+    server_addr = "ws://192.168.1.77:2777"
+    try: 
         async with websockets.connect(server_addr) as websocket:
-            with open("image-for-processing.jpg", "rb") as image_file:
-                image_data = image_file.read()
-                image_data = base64.b64encode(image_data).decode("utf-8")
-                await websocket.send(f'IMAGE:{image_data}')
+            if os.path.exists(args.data):
+                await send_image(websocket, args.data)
+            else:
+                print(f"Error: The file '{args.data}' does not exist.")
+                return
     except websockets.exceptions.ConnectionClosedError:
         print(f'Connection to {server_addr} Closed Unexpectedly')
     except websockets.exceptions.InvalidURI:
@@ -21,4 +34,5 @@ async def send_message():
     except Exception as e:
         print(f'An Unexpected Error Occurred: {e}')
 
-asyncio.run(send_message())
+if __name__ == "__main__":
+    asyncio.run(main())
